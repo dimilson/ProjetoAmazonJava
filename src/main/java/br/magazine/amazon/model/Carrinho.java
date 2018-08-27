4d8a9676651a43ad3770325147d1e025a263c149
Luiz <davidaugusto.ifal@gmail.com>
2018-08-27 09:18:01 -0300

    criação de uma arrayList para guardar todos os produtos do cliente / mapeamento da classe Carrinho.java

--- src/main/java/br/magazine/amazon/model/Carrinho.java
+++ src/main/java/br/magazine/amazon/model/Carrinho.java
@@ -1,6 +1,33 @@
 package br.magazine.amazon.model;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
 public class Carrinho {
+	
+	@Id
+	@GeneratedValue
+	private Long id ;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo")
+	private List<Produto> produto = new ArrayList<>();
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
