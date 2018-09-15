e12fe5ed15a79ff381a3af3b106569342643843f
David Augusto <davidaugusto.ifal@gmail.com>
2018-09-15 01:29:28 -0300

    criação do pacote telas e nova classe cadastro

---
+++ src/main/java/br/magazine/amazon/telas/CadastroDeCliente.java
@@ -0,0 +1,94 @@
+package br.magazine.amazon.telas;
+
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import br.magazine.amazon.JPAUtil;
+import br.magazine.amazon.model.Cliente;
+import br.magazine.amazon.model.Endereco;
+
+
+public class CadastroDeCliente {
+	
+	public void cadastrarCliente() {
+		
+		Scanner ler = new Scanner(System.in);
+		
+		String nome ;
+		String estado ;
+		String cidade;
+		String cep;
+		String rua ;
+		String bairro;
+		String email ;
+		String senha;
+		String telefone;
+		
+		
+		
+		System.out.println("Digite seu nome completo: ");
+		nome = ler.nextLine();
+		
+		System.out.println("Digite o estado onde mora: ");
+		estado = ler.nextLine();
+		
+		System.out.println("Digite a cidade onde mora: ");
+		cidade = ler.nextLine();
+		
+		System.out.println("Digite seu cep: ");
+		cep = ler.nextLine();
+		
+		System.out.println("Digite seu logradouro: ");
+		rua = ler.nextLine();
+		
+		System.out.println("Digite o nome do seu bairro: ");
+		bairro = ler.nextLine();
+		
+		System.out.println("Digite um email: ");
+		email = ler.nextLine();
+		
+		System.out.println("Digite uma senha: ");
+		senha = ler.nextLine();
+		
+		System.out.println("Digite seu telefone de contato: ");
+		telefone = ler.nextLine();
+
+		
+		EntityManager manager = JPAUtil.getEntityManager();
+		EntityTransaction tx = manager.getTransaction();
+		
+		tx.begin();
+		
+		
+		Cliente pessoa = new Cliente(); 
+		Endereco endereco = new Endereco(estado, cidade, bairro, cep, rua);
+		
+		manager.persist(endereco);
+		
+		
+		pessoa.setNome(nome);
+		pessoa.setEmail(email);
+		pessoa.getEnderecos().add(endereco);
+		pessoa.setTelefone(telefone);
+		pessoa.setSenha(senha);
+		
+		
+		manager.persist(pessoa);
+		
+		
+		tx.commit();
+		manager.close();
+		JPAUtil.close();
+		
+		
+		
+	}
+
+	
+	
+	
+	
+
+}
