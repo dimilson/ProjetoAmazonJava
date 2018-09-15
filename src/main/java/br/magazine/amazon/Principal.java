54d5746eb0073868dbf9fb7cb689f0ccf59db6a9
David Augusto <davidaugusto.ifal@gmail.com>
2018-09-15 00:20:10 -0300

    adição da chamada do cadastro de cliente

--- src/main/java/br/magazine/amazon/Principal.java
+++ src/main/java/br/magazine/amazon/Principal.java
@@ -1,11 +1,13 @@
 package br.magazine.amazon;
 
+import cadastrar.CadastroDeCliente;
 
 public class Principal {
 	
 	public static void main(String[] args) {
 		
-	
+		CadastroDeCliente.cadastrarCliente();
+		
 	}
 
 }
